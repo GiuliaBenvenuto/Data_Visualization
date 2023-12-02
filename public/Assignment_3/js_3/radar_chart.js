311ef470752f3d600ec8a8ac00592868fd7998a3
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2023-12-02 17:29:09 +0100

    radar chart titles

--- public/Assignment_3/js_3/radar_chart.js
+++ public/Assignment_3/js_3/radar_chart.js
@@ -86,7 +86,7 @@ function updateRadarChart(selectedOption) {
     }
 
 
-    var margin = {top: 100, right: 100, bottom: 100, left: 100},
+    var margin = {top: 80, right: 100, bottom: 100, left: 100},
         width = Math.min(400, window.innerWidth - 10) - margin.left - margin.right,
         height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
         //console.log("WIDTH", window.innerWidth);
